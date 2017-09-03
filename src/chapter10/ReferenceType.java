ca8c0598f9344f664d2fdb3273595fe1225e408f
郑军 <zhengjun1987@outlook.com>
2017-09-03 18:52:18 +0800

    2017-09-03 18:52:13

--- src/chapter10/ReferenceType.java
+++ src/chapter10/ReferenceType.java
@@ -15,6 +15,13 @@ public class ReferenceType {
         proceessPerson(person);
         System.out.println(person.getName());
         System.out.println("2017年07月13日".substring(0,8));
+        System.out.println("____________________________________");
+        System.out.print("慕容雪村\t");
+        System.out.print("鲁迅\t");
+        System.out.print("曹雪芹\t\n");
+        System.out.print("吴承恩\t");
+        System.out.print("施耐庵\t");
+        System.out.print("罗贯中\t\n");
     }
 
     static void proceessPerson(Person person){
