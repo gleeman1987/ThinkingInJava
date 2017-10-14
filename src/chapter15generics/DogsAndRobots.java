1cb0e59fd5d1559962415880ee52c83ff82339a8
郑军 <zhengjun1987@outlook.com>
2017-10-14 22:22:11 +0800

    <Thinking In Java>第四版 第十五章 泛型 2017-10-13 00:57:22

---
+++ src/chapter15generics/DogsAndRobots.java
@@ -0,0 +1,41 @@
+package chapter15generics;
+
+import chapter14runtimetypeinfo.Dog;
+
+/**
+ * OkLine(Hangzhou) Co,ltd
+ * Created by zhengjun
+ * Date: 2017/10/14
+ * Mail:zhengjun1987@outlook.com
+ */
+interface Performs {
+    void speak();
+    void sit();
+}
+class PerformingDog extends Dog implements Performs {
+    @Override
+    public void speak() {
+        System.out.println("PerformingDog.bark Woof!");
+    }
+
+    @Override
+    public void sit() {
+        System.out.println("PerformingDog.sit");
+    }
+}
+
+class Robot implements Performs {
+    @Override
+    public void speak() {
+        System.out.println("Robot.speak 'Click!'");
+    }
+
+    @Override
+    public void sit() {
+        System.out.println("Robot.sit");
+    }
+}
+
+public class DogsAndRobots {
+
+}
