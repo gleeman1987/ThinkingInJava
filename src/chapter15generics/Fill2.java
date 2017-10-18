cebd84c5663d39b056d7bc11664c8a8d5558bd11
郑军 <zhengjun1987@outlook.com>
2017-10-19 00:17:32 +0800

    <Thinking In Java>第四版 第十五章 泛型 2017-10-19 00:17:22

--- src/chapter15generics/Fill2.java
+++ src/chapter15generics/Fill2.java
@@ -1,5 +1,9 @@
 package chapter15generics;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 /**
  * OkLine(Hangzhou) Co,ltd
  * Created by zhengjun
@@ -26,5 +30,57 @@ public class Fill2 {
         for (int i = 0; i < size; i++) {
             addable.add(generator.next());
         }
+    }
+}
+
+class AddableCollectionAdapter<T> implements Addable<T>{
+    private Collection<T> collection;
+
+    public AddableCollectionAdapter(Collection<T> collection) {
+        this.collection = collection;
+    }
+
+    @Override
+    public void add(T t) {
+        collection.add(t);
+    }
+}
+
+class Adapter {
+    public static <T> Addable<T> collectionAdapter(Collection<T> collection){
+        return new AddableCollectionAdapter<>(collection);
+    }
+}
+
+class AddableSimpleQueue<T> extends SimpleQueue<T> implements Addable<T>{
+    @Override
+    public void add(T t) {
+        super.add(t);
+    }
+}
+
+class Fill2Test {
+    public static void main(String[] args) {
+        List<Coffe> coffes = new ArrayList<>();
+        Fill2.fill(Adapter.collectionAdapter(coffes),Coffe.class,3);
+        Fill2.fill(new AddableCollectionAdapter<Coffe>(coffes),Latte.class,2);
+        System.out.println("coffes = " + coffes);
+        System.out.println("-----------------------");
+        AddableSimpleQueue<Coffe> coffeAddableSimpleQueue = new AddableSimpleQueue<>();
+        Fill2.fill(coffeAddableSimpleQueue,Mocha.class,4);
+        Fill2.fill(coffeAddableSimpleQueue,Americano.class,4);
+        for (Coffe coffe : coffeAddableSimpleQueue) {
+            System.out.println("coffe = " + coffe);
+        }
+//        coffes = [Coffe 0, Coffe 1, Coffe 2, Latte 3, Latte 4]
+//        -----------------------
+//        coffe = Mocha 5
+//        coffe = Mocha 6
+//        coffe = Mocha 7
+//        coffe = Mocha 8
+//        coffe = Americano 9
+//        coffe = Americano 10
+//        coffe = Americano 11
+//        coffe = Americano 12
     }
 }
